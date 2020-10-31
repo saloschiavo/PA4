d350be247a0e0c455daa552236d36050d454e215
Stefanie <saloschiavo@gmail.com>
2020-10-31 01:01:09 -0700

    Create PA4Tests.py
    very basic template for adding unit tests as I go

---
+++ PA4Tests.py
@@ -0,0 +1,25 @@
+# TODO: Give unit testing a try on your own with this one; they can be really powerful for development
+# TODO: For example: make one test where you put some items in and there's no colisions
+# TODO: Then make one test where there's colisions on one slot
+# TODO: Then make one where there's colisions on multiple spots
+
+
+from Map import Map
+from HashTable import HashTable
+from WordPredictor import WordPredictor
+import unittest
+
+
+class PA4Tests(unittest.TestCase):
+    def test_put(self):
+        hashset = HashTable()
+        hashset.put(1)
+        hashset.put(9)
+        hashset.put(4)
+        hashset.put(6)
+
+        # TODO: Modify this for the format of the HashSet
+        self.assertEqual(str(hashset), "1, 4, 6, 9")
+
+
+unittest.main()
